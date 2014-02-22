15da34d7d3a7cc45914fda92d0a35f9b21092f84
James Ward <james@jamesward.org>
2014-02-22 11:55:50 -0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+WebJar for angular-blocks
+
+More info: http://webjars.org
+
+Upstream: https://github.com/wmluke/angular-blocks
---
+++ src/main/resources/webjars-requirejs.js
@@ -0,0 +1,4 @@
+requirejs.config({
+    paths: { "angular-blocks": webjars.path("angular-blocks", "angular-blocks") },
+    shim: { "angular-blocks": [ "angular" ] }
+});
